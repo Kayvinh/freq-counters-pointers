442718e400880b15b0a735e7424ed96ad3e76950
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-24 18:44:36 -0500

    add: can-construct-word

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,38 @@
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+function canConstructWord(word, letters) {
+  // check for length of letters, if word longer than letters, is false
+  if (word.length > letters.length) return false;
+  // build freq counter of letters available
+  const letterCount = getFreqCounter(letters);
+  // iterate over word
+  for (const letter of word) {
+    // check if that letter is in the letters freq counter
+    if (letterCount[letter] > 0) {
+    // if so, decrement our freq for letters
+      letterCount[letter]--;
+    } else {
+    // if not return false
+    return false;
+    }
+  }
+  //return true
+  return true;
+}
+
+canConstructWord('aa', 'abc');
+// false -- can't build "aa" with only 1 "a"
+
+canConstructWord('abc', 'dcba');
+  // true -- can build "abc" with letters "abcd"
+
+canConstructWord('aabb', 'bcabad');
+  // true -- can build "aabbcc" with those letters
+
+function getFreqCounter(items) {
+  let freqs = {};
+  for (let item of items) {
+    let value = freqs[item] || 0;
+    freqs[item] = value + 1;
+  }
+  return freqs;
 }
