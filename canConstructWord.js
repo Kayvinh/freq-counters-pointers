c69c7509e5a66996e406ffdd98beb343c90cebfc
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-24 19:26:53 -0500

    add: canConstructWord

---
+++ canConstructWord.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/** Function takes two strings:  word and letters, checks to see
+ * if the word can be made out of letters given. Returns boolean.
+*/
+// add whatever parameters you deem necessary & write doc comment
+function canConstructWord(word, letters) {
+  // check for length of letters, if word longer than letters, is false
+  if (word.length > letters.length) return false;
+  // build freq counter of letters available
+  const letterCount = getFreqCounter(letters);
+  // iterate over word
+  for (const letter of word) {
+    // check if that letter is in the letters freq counter
+    if (letterCount[letter] > 0) {
+    // if so, decrement our freq for letters
+      letterCount[letter]--;
+    } else {
+    // if not return false
+    return false;
+    }
+  }
+  //return true
+  return true;
+}
+
+function getFreqCounter(items) {
+  let freqs = {};
+  for (let item of items) {
+    let value = freqs[item] || 0;
+    freqs[item] = value + 1;
+  }
+  return freqs;
+}
