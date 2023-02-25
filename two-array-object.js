7e98b06323533f2c7106a35a0017dad00ca3ff34
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-02-24 17:22:33 -0800

    two-array-object completed

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,24 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+/**
+ * Accepts 2 params keys, and values, and sets keys to value
+ * if values length is greater than keys length, set keys value to null
+ * @param {*} keys is array
+ * @param {*} values is array
+ * @returns object with keys and value pair
+ */
+
+function twoArrayObject(keys, values) {
+    let twoObject = {};
+
+    for (let i = 0; i < keys.length; i++) {
+        if (values[i] === undefined) {
+            console.log('check if ran');
+            twoObject[keys[i]] = null;
+        } else {
+            twoObject[keys[i]] = values[i];
+        }
+    }
+
+    return twoObject;
 }
